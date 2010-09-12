5354ce88df06a49db805be0d26cbe2e4a7968068
Jack Palevich <jack.palevich@gmail.com>
2010-09-12 06:31:51 -0700

    First working Javascript robot.

---
+++ MyBot.js
@@ -0,0 +1,62 @@
+/*
+// The DoTurn function is where your code goes. The PlanetWars object contains
+// the state of the game, including information about all planets and fleets
+// that currently exist. Inside this function, you issue orders using the
+// pw.IssueOrder() function. For example, to send 10 ships from planet 3 to
+// planet 8, you would say pw.IssueOrder(3, 8, 10).
+//
+// There is already a basic strategy in place here. You can use it as a
+// starting point, or you can throw it out entirely and replace it with your
+// own. Check out the tutorials and articles on the contest website at
+// http://www.ai-contest.com/resources.
+*/
+
+var PlanetWars = require('./PlanetWars');
+
+function DoTurn(pw) {
+    // (1) If we currently have a fleet in flight, just do nothing.
+    if ( pw.myFleets.length >= 1 ) {
+        return;
+    }
+    // (2) Find my strongest planet.
+    var source = -1;
+    var score;
+    var source_score = -999999.0;
+    var sourceShips = 0;
+    var myPlanets = pw.myPlanets;
+    var p, pi, plen;
+    var dest, destScore, notMyPlanets;
+    plen = myPlanets.length;
+    for (pi = 0; pi < plen; pi++) {
+        p = myPlanets[pi];
+        score = p.ships;
+        if (score > source_score ) {
+            source_score = score;
+            source = p.id;
+            sourceShips = p.ships;
+        }
+    }
+
+    // (3) Find the weakest enemy or neutral planet.
+    dest = -1;
+    destScore = -999999.0;
+    notMyPlanets = pw.notMyPlanets;
+    plen = notMyPlanets.length;
+    for (pi = 0; pi < plen; pi++) {
+        p = notMyPlanets[pi];
+        score = 1.0 / (1 + p.ships);
+        if (score > destScore) {
+            destScore = score;
+            dest = p.id;
+        }
+    }
+    // (4) Send half the ships from my strongest planet to the weakest
+    // planet that I do not own.
+    if ( source >= 0 && dest >= 0 ) {
+        num_ships = Math.floor(sourceShips / 2);
+        pw.IssueOrder(source, dest, num_ships);
+    }
+}
+
+// Play the game with my bot
+PlanetWars.Play(DoTurn);
---
+++ PlanetWars.js
@@ -0,0 +1,118 @@
+var sys = require('sys');
+
+function Planet(id, x, y, owner, ships, growth) {
+    return {id:id, x:x, y:y, owner:owner, ships: ships, growth: growth};
+}
+
+function Fleet(id, owner, ships, source, dest, totalLength, remaining) {
+    return {id:id, owner : owner,
+        ships: ships, source: source, dest: dest,
+        totalLength: totalLength, remaining: remaining};
+}
+
+function Universe(planets, fleets) {
+    var i;
+    var planetsLength = planets.length;
+    var myPlanets = [];
+    var enemyPlanets = [];
+    var neutralPlanets = [];
+    var planet;
+    var planetsByOwner = [neutralPlanets, myPlanets, enemyPlanets];
+    var owner;
+    for (i = 0; i < planetsLength; i++) {
+        planet = planets[i];
+        owner = planet.owner;
+        if (owner < 0 || owner >= planetsByOwner.length) {
+            throw "Unknown planet owner " + owner;
+        }
+        planetsByOwner[owner].push(planet);
+    }
+
+    var myFleets = [];
+    var enemyFleets = [];
+    var fleetsLength = fleets.length;
+    var fleetsByOwner = [myFleets, enemyFleets];
+    var fleet;
+    for (i = 0; i < fleetsLength; i++) {
+        fleet = fleets[i];
+        owner = fleet.owner;
+        if (owner < 1 || owner > 2) {
+            throw "Unknown fleet owner " + owner;
+        }
+        fleetsByOwner[owner-1].push(fleet);
+    }
+
+    return {
+        planets: planets,
+        neutralPlanets:neutralPlanets,
+        myPlanets: myPlanets,
+        enemyPlanets: enemyPlanets,
+        notMyPlanets: neutralPlanets.concat(enemyPlanets),
+        notEnemyPlanets: neutralPlanets.concat(myPlanets),
+        notNeutralPlanets: myPlanets.concat(enemyPlanets),
+
+        fleets: fleets,
+        myFleets: myFleets,
+        enemyFleets: enemyFleets,
+
+        IssueOrder: function IssueOrder(source, dest, ships) {
+            process.stdout.write('' + Math.floor(source) + ' '
+                    + Math.floor(dest) + ' ' + Math.floor(ships) + '\n');
+        }
+    };
+}
+
+function parseInput(turnInput, turnFn) {
+    var lines = turnInput.split('\n');
+    var linesLength = lines.length;
+    var i;
+    var line;
+    var noCommentLine;
+    var planets = [];
+    var fleets = [];
+    var toks;
+    var cmd;
+    var universe;
+    for (i = 0; i < linesLength; i++) {
+        line = lines[i];
+        noCommentLine = line.split('#')[0];
+        toks = noCommentLine.split(' ');
+        cmd = toks[0];
+        switch (cmd) {
+        case 'P':
+            planets.push(Planet(planets.length,
+                    toks[1], toks[2], toks[3], toks[4], toks[5]));
+            break;
+        case 'F':
+            fleets.push(Fleet(fleets.length,
+                    toks[1], toks[2], toks[3], toks[4], toks[5], toks[6]));
+            break;
+        default:
+            throw "Unknown command token: " + line;
+        }
+    }
+    universe = Universe(planets, fleets);
+    turnFn(universe);
+    process.stdout.write('go\n');
+}
+
+exports.Play = function Play(turnFn) {
+    var stdin = process.openStdin();
+    var stdout = process.stdout;
+    var buffer = '';
+    sys.debug('Play()');
+    stdin.on('data', function (chunk) {
+        buffer += chunk;
+        var endOfTurn = buffer.indexOf('\ngo\n');
+        if (endOfTurn >= 0) {
+            var turnInput = buffer.substring(0, endOfTurn);
+            buffer = buffer.substring(endOfTurn + 4);
+            parseInput(turnInput, turnFn);
+        }
+    });
+
+    stdin.on('end', function () {
+      sys.debug('end of stdin, exiting');
+      sys.exit();
+    });
+};
