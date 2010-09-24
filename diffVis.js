842ad3f5f6ff445d3738b00887ab8022eef71cdc
Jack Palevich <jack.palevich@gmail.com>
2010-09-24 05:21:15 -0700

    Start of a visualization difference utility

---
+++ diffVis.js
@@ -0,0 +1,33 @@
+// Compare two game visualation files
+var fs = require('fs');
+var argv = process.argv.slice(2);
+
+function readFile(path) {
+    var txt = fs.readFileSync(path,'utf8');
+    var parts = txt.split('|');
+    var start = parts[0];
+    var turns = parts[1];
+    return start.split(':').concat(turns.split(':'));
+}
+
+var a = readFile(argv[0]);
+var b = readFile(argv[1]);
+
+console.log("length: " + a.length + ":" + b.length);
+
+a.forEach(function(v,i,a) {
+	var la = v;
+	var lb = b[i];
+	if (la != lb) {
+	    console.log(i + ":");
+	    console.log(la);
+	    console.log(lb);
+	    var n = la.length;
+	    var j;
+	    for (j = 0; j < n; j++) {
+		if(la[j] != lb[j]) {
+		    console.log(j + '!' + la[j] + '!=' + lb[j]);
+		}
+	    }
+	}
+    });
